e5659698ba2a3a66514411ec708a845490138246
Miguel Antony Martinez Mora <antonymartinez12@gmail.com>
2016-10-16 16:15:46 -0500

    Implementado slide de imagenes en HomeRetailFragment

--- app/src/main/java/com/martinez/apptiendaonline/HomeRetailFragment.java
+++ app/src/main/java/com/martinez/apptiendaonline/HomeRetailFragment.java
@@ -2,24 +2,123 @@ package com.martinez.apptiendaonline;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.ViewAnimator;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 
-
-public class HomeRetailFragment extends Fragment
+public class HomeRetailFragment extends Fragment implements View.OnClickListener
 {
+    //ViewAnimator
+    private ViewAnimator viewAnimator;
+    //Animaciones
+    private Animation slide_in_left, slide_out_right;
+    //Slides
+    private ImageButton slide1;
+    private ImageButton slide2;
+    private ImageButton slide3;
+
+    //Timer
+    Timer timer = null;
+    private int count = 0;
+
 
     public HomeRetailFragment()
     {
 
     }
 
+    @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState )
     {
         //Recibimos el inflater de la actividad padre y le inflamos el layout de este fragment
-        return inflater.inflate( R.layout.fragment_home_retail_, container, false);
+        View root = inflater.inflate( R.layout.fragment_home_retail_, container, false);
+
+        //Referenciamos los views
+        viewAnimator = (ViewAnimator)root.findViewById(R.id.viewAnimator);
+        slide1 = (ImageButton)root.findViewById(R.id.slide1);
+        slide2 = (ImageButton)root.findViewById(R.id.slide2);
+        slide3 = (ImageButton)root.findViewById(R.id.slide3);
+
+        slide1.setOnClickListener(this);
+        slide2.setOnClickListener(this);
+        slide3.setOnClickListener(this);
+
+
+
+        //Animaciones
+        slide_in_left = AnimationUtils.loadAnimation( getContext() ,android.R.anim.slide_in_left);
+        slide_out_right = AnimationUtils.loadAnimation( getContext(),android.R.anim.slide_out_right);
+        viewAnimator.setInAnimation( slide_in_left );
+        viewAnimator.setOutAnimation( slide_out_right );
+
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(
+
+                new TimerTask()
+                {
+                    @Override
+                    public void run()
+                    {
+                        viewAnimator.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                viewAnimator.setDisplayedChild(count);
+                                count++;
+                                if(count>2)
+                                    count = 0;
+                            }
+                        }, 2000);
+                        /*
+                        getActivity().runOnUiThread(
+
+                                new Runnable()
+                                {
+                                    @Override
+                                    public void run()
+                                    {
+                                        viewAnimator.setDisplayedChild(count);
+                                        count++;
+                                        if(count>2)
+                                            count = 0;
+                                    }
+                                }
+
+                        );*/
+                    }
+                }
+
+        , 0, 2000);
+
+
+
+        return root;
     }
 
+    @Override
+    public void onClick(View v)
+    {
+        switch( v.getId() )
+        {
+            case R.id.slide1:
+                Toast.makeText(getActivity().getApplicationContext(), "Presionaste slide 1", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.slide2:
+                Toast.makeText(getActivity().getApplicationContext(), "Presionaste slide 2", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.slide3:
+                Toast.makeText(getActivity().getApplicationContext(), "Presionaste slide 3", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/martinez/apptiendaonline/MyFragmentPagerAdapter.java
@@ -0,0 +1,7 @@
+package com.martinez.apptiendaonline;
+
+/**
+ * Created by HP on 10/15/2016.
+ */
+public class MyFragmentPagerAdapter {
+}
